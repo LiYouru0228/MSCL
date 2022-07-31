5c2a361e56d3e52742ded6c4e82c549535686684
LiYouru0228 <liyouru0228@126.com>
2022-07-31 14:23:15 +0800

    Create model.py

---
+++ src/model.py
@@ -0,0 +1,301 @@
+import numpy as np
+import argparse
+import os
+import imp
+import re
+import pickle
+import datetime
+import random
+import math
+import copy
+
+import torch
+from torch import nn
+import torch.nn.utils.rnn as rnn_utils
+from torch.utils import data
+from torch.autograd import Variable
+import torch.nn.functional as F
+import torch
+from torch import nn
+import torch.nn.utils.rnn as rnn_utils
+from torch.utils import data
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+device = torch.device("cuda:0" if torch.cuda.is_available() == True else 'cpu')
+print("available device: {}".format(device))
+
+class FinalAttentionQKV(nn.Module):
+    def __init__(self, attention_input_dim, attention_hidden_dim, attention_type='add', dropout=None):
+        super(FinalAttentionQKV, self).__init__()
+        
+        self.attention_type = attention_type
+        self.attention_hidden_dim = attention_hidden_dim
+        self.attention_input_dim = attention_input_dim
+
+
+        self.W_q = nn.Linear(attention_input_dim, attention_hidden_dim)
+        self.W_k = nn.Linear(attention_input_dim, attention_hidden_dim)
+        self.W_v = nn.Linear(attention_input_dim, attention_hidden_dim)
+
+        self.W_out = nn.Linear(attention_hidden_dim, 1)
+
+        self.b_in = nn.Parameter(torch.zeros(1,))
+        self.b_out = nn.Parameter(torch.zeros(1,))
+
+        nn.init.kaiming_uniform_(self.W_q.weight, a=math.sqrt(5))
+        nn.init.kaiming_uniform_(self.W_k.weight, a=math.sqrt(5))
+        nn.init.kaiming_uniform_(self.W_v.weight, a=math.sqrt(5))
+        nn.init.kaiming_uniform_(self.W_out.weight, a=math.sqrt(5))
+
+        self.Wh = nn.Parameter(torch.randn(2*attention_input_dim, attention_hidden_dim))
+        self.Wa = nn.Parameter(torch.randn(attention_hidden_dim, 1))
+        self.ba = nn.Parameter(torch.zeros(1,))
+        
+        nn.init.kaiming_uniform_(self.Wh, a=math.sqrt(5))
+        nn.init.kaiming_uniform_(self.Wa, a=math.sqrt(5))
+        
+        self.dropout = nn.Dropout(p=dropout)
+        self.tanh = nn.Tanh()
+        self.softmax = nn.Softmax()
+        self.sigmoid = nn.Sigmoid()
+    
+    def forward(self, input):
+ 
+        batch_size, time_step, input_dim = input.size() # batch_size * input_dim + 1 * hidden_dim(i)
+        input_q = self.W_q(input[:, -1, :]) # b h
+        input_k = self.W_k(input)# b t h
+        input_v = self.W_v(input)# b t h
+
+        if self.attention_type == 'add': #B*T*I  @ H*I
+
+            q = torch.reshape(input_q, (batch_size, 1, self.attention_hidden_dim)) #B*1*H
+            h = q + input_k + self.b_in # b t h
+            h = self.tanh(h) #B*T*H
+            e = self.W_out(h) # b t 1
+            e = torch.reshape(e, (batch_size, time_step))# b t
+
+        elif self.attention_type == 'mul':
+            q = torch.reshape(input_q, (batch_size, self.attention_hidden_dim, 1)) #B*h 1
+            e = torch.matmul(input_k, q).squeeze()#b t
+            
+        elif self.attention_type == 'concat':
+            q = input_q.unsqueeze(1).repeat(1,time_step,1)# b t h
+            k = input_k
+            c = torch.cat((q, k), dim=-1) #B*T*2I
+            h = torch.matmul(c, self.Wh)
+            h = self.tanh(h)
+            e = torch.matmul(h, self.Wa) + self.ba #B*T*1
+            e = torch.reshape(e, (batch_size, time_step)) # b t 
+        
+        a = self.softmax(e) #B*T
+        if self.dropout is not None:
+            a = self.dropout(a)
+        v = torch.matmul(a.unsqueeze(1), input_v).squeeze() #B*I
+
+        return v, a
+
+def clones(module, N):
+    "Produce N identical layers."
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+def tile(a, dim, n_tile):
+    init_dim = a.size(dim)
+    repeat_idx = [1] * a.dim()
+    repeat_idx[dim] = n_tile
+    a = a.repeat(*(repeat_idx))
+    order_index = torch.LongTensor(np.concatenate([init_dim * np.arange(n_tile) + i for i in range(init_dim)])).to(device)
+    return torch.index_select(a, dim, order_index).to(device)
+
+class PositionwiseFeedForward(nn.Module): # new added
+    "Implements FFN equation."
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.w_2(self.dropout(F.relu(self.w_1(x)))), None
+
+class PositionalEncoding(nn.Module): # new added / not use anymore
+    "Implement the PE function."
+    def __init__(self, d_model, dropout, max_len=400):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0., max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0., d_model, 2) * -(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+        
+    def forward(self, x):
+        x = x + Variable(self.pe[:, :x.size(1)], 
+                         requires_grad=False)
+        return self.dropout(x)
+
+def subsequent_mask(size):
+    "Mask out subsequent positions."
+    attn_shape = (1, size, size)
+    subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+    return torch.from_numpy(subsequent_mask) == 0 # 下三角矩阵
+
+def attention(query, key, value, mask=None, dropout=None):
+    "Compute 'Scaled Dot Product Attention'"
+    d_k = query.size(-1)# b h t d_k
+    scores = torch.matmul(query, key.transpose(-2, -1)) \
+             / math.sqrt(d_k) # b h t t
+    if mask is not None:# 1 1 t t
+        scores = scores.masked_fill(mask == 0, -1e9)# b h t t 下三角
+    p_attn = F.softmax(scores, dim = -1)# b h t t
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+    return torch.matmul(p_attn, value), p_attn # b h t v (d_k) 
+
+def loss_dependence(emb1, emb2, dim):
+    R = torch.eye(dim).to(device) - (1/dim) * torch.ones(dim, dim).to(device)
+    K1 = torch.mm(emb1, emb1.t()).to(device)
+    K2 = torch.mm(emb2, emb2.t()).to(device)
+    RK1 = torch.mm(R, K1).to(device)
+    RK2 = torch.mm(R, K2).to(device)
+    HSIC = torch.trace(torch.mm(RK1, RK2)).to(device)
+    return 1e-9 * HSIC
+    
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, h, d_model, dropout=0):
+        "Take in model size and number of heads."
+        super(MultiHeadedAttention, self).__init__()
+        assert d_model % h == 0
+        # We assume d_v always equals d_k
+        self.d_k = d_model // h
+        self.h = h
+        self.linears = clones(nn.Linear(d_model, self.d_k * self.h), 3)
+        self.final_linear = nn.Linear(d_model, d_model)
+        self.attn = None
+        self.dropout = nn.Dropout(p=dropout)
+        
+    def forward(self, query, key, value, mask=None):
+        if mask is not None:
+            # Same mask applied to all h heads.
+            mask = mask.unsqueeze(1) # 1 1 t t
+
+        nbatches = query.size(0)# b
+        input_dim = query.size(1)# i+1
+        feature_dim = query.size(-1)# i+1
+
+        #input size -> # batch_size * d_input * hidden_dim
+        
+        # d_model => h * d_k 
+        query, key, value = \
+            [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
+             for l, x in zip(self.linears, (query, key, value))] # b num_head d_input d_k
+        
+       
+        x, self.attn = attention(query, key, value, mask=mask, 
+                                 dropout=self.dropout)# b num_head d_input d_v (d_k) 
+
+      
+        x = x.transpose(1, 2).contiguous() \
+             .view(nbatches, -1, self.h * self.d_k)# batch_size * d_input * hidden_dim
+
+        #HSIC
+        DeCov_contexts = x.transpose(0, 1).transpose(1, 2) # I+1 H B
+        DeCov_loss = loss_dependence(DeCov_contexts[0,:,:],DeCov_contexts[0,:,:],DeCov_contexts.shape[1])
+                
+        feature_dim = x.shape[1]
+        for i in range(feature_dim -1):
+            DeCov_loss += loss_dependence(DeCov_contexts[i+1,:,:],DeCov_contexts[i+1,:,:],DeCov_contexts.shape[1])
+
+        return self.final_linear(x), DeCov_loss
+
+class LayerNorm(nn.Module):
+    def __init__(self, features, eps=1e-7):
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
+class SublayerConnection(nn.Module):
+    """
+    A residual connection followed by a layer norm.
+    Note for code simplicity the norm is first as opposed to last.
+    """
+    def __init__(self, size, dropout):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        "Apply residual connection to any sublayer with the same size."
+        returned_value = sublayer(self.norm(x))
+        return x + self.dropout(returned_value[0]) , returned_value[1]
+        
+class MSCL(nn.Module):
+    def __init__(self, input_dim, hidden_dim, d_model, MHD_num_head, d_ff, output_dim, keep_prob=0.5):
+        super(MSCL, self).__init__()
+
+        # hyperparameters
+        self.input_dim = input_dim  
+        self.hidden_dim = hidden_dim  # d_model
+        self.d_model = d_model
+        self.MHD_num_head = MHD_num_head
+        self.d_ff = d_ff
+        self.output_dim = output_dim
+        self.keep_prob = keep_prob
+
+        # layers
+        self.PositionalEncoding = PositionalEncoding(self.d_model, dropout = 0, max_len = 400)
+        
+        self.FinalAttentionQKV = FinalAttentionQKV(self.hidden_dim, self.hidden_dim, attention_type='mul',dropout = 1 - self.keep_prob)
+
+        self.MultiHeadedAttention = MultiHeadedAttention(self.MHD_num_head, self.d_model,dropout = 1 - self.keep_prob)
+        self.SublayerConnection = SublayerConnection(self.d_model, dropout = 1 - self.keep_prob)
+
+        self.PositionwiseFeedForward = PositionwiseFeedForward(self.d_model, self.d_ff, dropout=0.1)
+        
+        self.pair_proj_main = nn.Linear(1352, self.hidden_dim)
+        self.output0 = nn.Linear(self.hidden_dim, self.hidden_dim)
+        self.output1 = nn.Linear(self.hidden_dim, self.output_dim)
+        
+        self.dropout = nn.Dropout(p = 1 - self.keep_prob)
+        self.tanh = nn.Tanh()
+        self.softmax = nn.Softmax()
+        self.sigmoid = nn.Sigmoid()
+        self.relu = nn.ReLU()
+
+    def forward(self, input, input2, each_epoch, step):
+        
+        # input shape [batch_size, path_num, emb_dim]
+        batch_size = input.size(0)
+        feature_dim = input2.size(1)
+        assert(feature_dim == self.input_dim)# input Tensor : 256 * 48 * 76
+        assert(self.d_model % self.MHD_num_head == 0)
+
+        # forward
+        pair_info_embedd = self.tanh(self.pair_proj_main(input)).unsqueeze(1)
+        Paths_embeded_input = input2
+        
+        Attention_embeded_input = torch.cat((Paths_embeded_input, pair_info_embedd), 1)
+        posi_input = self.dropout(Attention_embeded_input) 
+
+        contexts = self.SublayerConnection(posi_input, lambda x: self.MultiHeadedAttention(posi_input, posi_input, posi_input, None)
+    
+        DeCov_loss = contexts[1]
+        contexts = contexts[0]
+
+        contexts = self.SublayerConnection(contexts, lambda x: self.PositionwiseFeedForward(contexts))[0]
+
+        weighted_contexts = self.FinalAttentionQKV(contexts)[0]
+        output = self.output1(self.relu(self.output0(weighted_contexts)))
+        output = self.sigmoid(output)
+          
+        return output, DeCov_loss
